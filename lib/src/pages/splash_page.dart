b49a7d0073d089c3044453093f64d3d101fd49e7
BrandonVS <vegasotobrandon@yahoo.com>
2022-01-29 23:52:22 -0500

    AnimacionSplash

--- lib/src/pages/splash_page.dart
+++ lib/src/pages/splash_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:remi/src/pages/home_page.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:remi/src/pages/login_page.dart';
-import 'package:remi/src/pages/properties_page.dart';
 
 class Splash extends StatefulWidget {
   Splash({Key key}) : super(key: key);
@@ -44,7 +43,11 @@ class _SplashState extends State<Splash> {
 
   _navigateHome() async {
     await Future.delayed(const Duration(milliseconds: 3000), () {});
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => LoginPage()));
+    Navigator.push(
+        context,
+        PageTransition(
+            type: PageTransitionType.rightToLeft,
+            duration: Duration(milliseconds: 750),
+            child: LoginPage()));
   }
 }
